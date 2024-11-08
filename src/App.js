e6792e1911641399107b5dc280578ba3f802388a
Archie Reader <archiereader@outlook.com>
2024-11-08 16:49:40 +0000

    sorted styling for the header, main body and footer.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Header from './components/Header';
 import Footer from './components/Footer';
 import About from './components/About';
 import Projects from './components/Projects';
-import Contact from './components/Contact';
 
 function App() {
     return (
@@ -13,7 +12,6 @@ function App() {
             <main>
                 <About />
                 <Projects />
-                <Contact />
             </main>
             <Footer />
         </div>
